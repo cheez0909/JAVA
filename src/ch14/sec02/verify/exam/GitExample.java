8711a2fb70f1be3d46324e8c60af29c883148261
dani <bin0696@naver.com>
2023-10-18 16:43:36 +0900

    1017에 GitExample 파일 추가

---
+++ src/ch14/sec02/verify/exam/GitExample.java
@@ -0,0 +1,10 @@
+package ch14.sec02.verify.exam;
+
+public class GitExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("깃허브연습중");
+	}
+
+}
